6daaf360f6d09683541f25783059efeb8a6977bf
Daniel Markham <danielbmarkham@hotmail.com>
2021-11-02 10:47:14 -0400

    need to check to see what mode we're in

--- ftest.fsx
+++ ftest.fsx
@@ -1,8 +1,20 @@
 #!/usr/bin/env -S dotnet fsi
 
+open System.Net
 open System
+
+
+let input =
+#if INTERACTIVE
+// TODO: Enter input here when running in F# Interactive
+        "duck"
+#else
+    System.Console.ReadLine(); 
+#endif
 
 printf "hello world\n"
 
 
 printf "how are you?\n"
+
+Console.WriteLine input
