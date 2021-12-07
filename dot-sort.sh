f8f72b141045a8c5a2a1ce7a9c554aa4dbae17fd
Daniel Markham <danielbmarkham@hotmail.com>
2021-12-07 06:03:51 -0500

    started adding some bash sorting work for diagram

---
+++ dot-sort.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# script to sort dot diagrams to drive automated biz dependency testing
+
+# what are the lines with business dependencies?
+grep '\->' VeterinaryClinic.pu | grep '\[color=red'
+
+# what are the lines with Process Area Business dependencies?
+grep '\->' VeterinaryClinic.pu | grep '\[color=red'| grep 'lhead'
+
+# what are the left/right for any connection?
+# grep '\->' VeterinaryClinic.pu | grep '\[color=red'| grep 'lhead' | grep -oEi '[a-z]+'
+
+# left pa deps including arrow
+grep '\->' VeterinaryClinic.pu | grep '\[color=red'| grep 'lhead' | grep -oEi '[a-z|A-Z|0-9]+\->'
+
+# right pa deps including arrow
+grep '\->' VeterinaryClinic.pu | grep '\[color=red'| grep 'lhead' | grep -oEi '\->[a-z|A-Z|0-9]+'
